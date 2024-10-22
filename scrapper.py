b0f1c3883e3e2c9d37d0cd4f2703bef5c217b9c8
Krishant <krishantsethia27@gmail.com>
2024-10-23 01:58:47 +0530

    removed the line causing error

--- scrapper.py
+++ scrapper.py
@@ -142,9 +142,6 @@ class LinkedInScraper:
         try:
             profile_posts_url = profile_url.rstrip('/') + '/recent-activity/all/'
             self.driver.get(profile_posts_url)
-            WebDriverWait(self.driver, 10).until(
-                lambda driver: driver.execute_script('return document.readyState') == 'complete'
-            )
             # Confirm that an expected element is present
             WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, 'div.feed-shared-update-v2'))
